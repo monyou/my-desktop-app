de06f536ea278f21382f5b9f6106ff3502cb08f9
Simeon Mechkov <montishak@gmail.com>
2026-06-24 05:05:50 +0300

    Release 1.0.3

--- electrobun.config.ts
+++ electrobun.config.ts
@@ -4,7 +4,7 @@ export default {
     app: {
         name: "my-desktop-app",
         identifier: "my-desktop-app.electrobun.dev",
-        version: "1.0.2",
+        version: "1.0.3",
     },
     build: {
         // Vite builds to dist/, we copy from there
